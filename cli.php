f4cd65bc1c1fc59853734a35d6e24e462639007c
Maks <khoroshunov.02@mail.ru>
2022-10-18 16:22:53 +0300

    Delete README.md

--- cli.php
+++ cli.php
@@ -2,10 +2,20 @@
 
 use App\Person\Name;
 use App\Blog\User;
+// include __DIR__ . "/vendor/autoload.php";
 
+spl_autoload_register('load');
 
-
-require_once __DIR__ . '/vendor/autoload.php';
+function load($class_name)
+{
+    $file = str_replace('_', DIRECTORY_SEPARATOR, strtolower($class_name)).'.php';
+    $file = str_replace("\\", "/", $file);
+    if ( ! file_exists($file))
+    {
+        return FALSE;
+    }
+    include $file;
+}
 //$faker = Faker\Factory::create('ru_RU');
 //
 //$name2 = new  Name(
